ebfa64fb575ad5b2475388c2b93769f092f7544e
Brenden Stahle <42918416+Brenden2008@users.noreply.github.com>
2023-10-03 16:28:23 -0500

    Add Version 1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # screenrecorder.js
 Script for JavaScript screen recording with microphone support.
+
+Depends on [RecordRTC](https://github.com/muaz-khan/RecordRTC)
+
+## API Usage
+
+### startRecord(doAudio = false: bool)
+```javascript
+// Start Screen Recording with Microphone Recording
+startRecord(true)
+// Start Screen Recording without Microphone Recording
+startRecord(false)
+// OR
+startRecord()
+```
+
+### stopRecord()
+```javascript
+// Stop a currently recording Screen Recording and save to disk
+stopRecord()
+```
---
+++ screenrecorder.js
@@ -0,0 +1,110 @@
+function invokeGetDisplayMedia(success, error) {
+    var displaymediastreamconstraints = {
+        video: {
+            displaySurface: 'monitor', // monitor, window, application, browser
+            logicalSurface: true,
+            cursor: 'always' // never, always, motion
+        }
+    };
+
+    // above constraints are NOT supported YET
+    // that's why overriding them
+    displaymediastreamconstraints = {
+        video: true
+    };
+
+    if (navigator.mediaDevices.getDisplayMedia) {
+        navigator.mediaDevices.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
+    }
+    else {
+        navigator.getDisplayMedia(displaymediastreamconstraints).then(success).catch(error);
+    }
+}
+
+function addStreamStopListener(stream, callback) {
+    stream.addEventListener('ended', function () {
+        callback();
+        callback = function () { };
+    }, false);
+    stream.addEventListener('inactive', function () {
+        callback();
+        callback = function () { };
+    }, false);
+    stream.getTracks().forEach(function (track) {
+        track.addEventListener('ended', function () {
+            callback();
+            callback = function () { };
+        }, false);
+        track.addEventListener('inactive', function () {
+            callback();
+            callback = function () { };
+        }, false);
+    });
+}
+
+function captureScreen(callback) {
+    invokeGetDisplayMedia(function (screen) {
+        addStreamStopListener(screen, function () {
+            stopRecord()
+        });
+        callback(screen);
+    }, function (error) {
+        console.error(error);
+        alert('Unable to capture your screen. Please check console logs.\n' + error);
+    });
+}
+
+function captureAudio(cb) {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(cb);
+}
+
+var globdoaudio = false;
+var globaudio
+
+function startRecord(doAudio = false) {
+    this.disabled = true;
+    if (doAudio) {
+        globdoaudio = true;
+        captureScreen(function (screen) {
+            captureAudio(function (audio) {
+                globaudio = audio;
+                recorder = RecordRTC([screen, audio], {
+                    type: 'video/webm;codecs=vp9',
+                    mimeType: 'video/webm;codecs=vp9',
+                });
+
+                recorder.startRecording();
+
+                // release screen on stopRecording
+                recorder.screen = screen;
+            });
+        });
+    } else {
+        captureScreen(function (screen) {
+            recorder = RecordRTC(screen, {
+                type: 'video/webm;codecs=vp9',
+                mimeType: 'video/webm;codecs=vp9',
+            });
+
+            recorder.startRecording();
+
+            // release screen on stopRecording
+            recorder.screen = screen;
+        });
+    }
+}
+
+function stopRecord() {
+    this.disabled = true;
+    recorder.stopRecording(function () {
+        if (globdoaudio) {
+            [globaudio].forEach(function (stream) {
+                stream.getTracks().forEach(function (track) {
+                    track.stop();
+                });
+            });
+        }
+        let blob = recorder.getBlob();
+        invokeSaveAsDialog(blob);
+    })
+}
